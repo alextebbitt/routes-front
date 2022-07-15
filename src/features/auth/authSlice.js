dea7fc3050b5595f63b77051a95688cdb1175e2b
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-15 14:11:10 +0200

    authslice and auth service

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,61 @@
+import authService from "./authService";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+
+
+const user = JSON.parse(localStorage.getItem("user"));
+const initialState = {
+    users: [],
+    user: user ? user : null,
+    isError: false,
+    isSuccess: false,
+    message: "",
+
+};
+
+
+export const register = createAsyncThunk("auth/register", async (user, thunkAPI) => {
+    try {
+        return await authService.register(user);
+    } catch (error) {
+
+        const message = error.response.data;
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+    try {
+        return await authService.login(user);
+    } catch (error) {
+        const message = error.response.data.message;
+        return thunkAPI.rejectWithValue(message);
+    }
+});
+
+export const logout = createAsyncThunk("auth/logout", async () => {
+    try {
+        return await authService.logout();
+    } catch (error) {
+        console.error(error);
+    }
+});
+export const authSlice = createSlice({
+
+    name: "auth",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+
+        builder
+            .addCase(login.fulfilled, (state, action) => {
+                state.user = action.payload;
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.user = null;
+            })
+
+    },
+})
+export default authSlice.reducer;
