763624d974b2efc7fc4b12b4d10c736f56c89c5c
Xavimat <xaviermatoses@gmail.com>
2022-07-15 20:38:56 +0200

    review profile

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+
+  const { user } = useSelector((state) => state.auth);
+
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Profile</h1>
+      <div>Name: {user.user.name}</div>
+      <div>Email: {user.user.email}</div>
+      <div>Role: {user.user.role}</div>
+    </div>
   )
 }
 
