8f773a1f5a9ee650a128b9fd66eeb1a85c89f085
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-15 12:12:11 +0200

    vista register and browser routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
   <Home/>
+  <Register/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
