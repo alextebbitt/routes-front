afaf2b33e66be52ff1adae213196f49c0b5765fe
Xavimat <xaviermatoses@gmail.com>
2022-07-15 21:21:55 +0200

    .end file

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL;
 
 const register = async (userData) => {
   const res = await axios.post(API_URL + "/users", userData);
