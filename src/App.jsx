a396f5a29a697c3776078b967c05b87b74f92e61
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-16 18:45:17 +0200

    component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Register from './components/Register/Register';
-import Login from './components/Login/Login';
-import Profile from './components/Profile/Profile';
+import Home from './Components/Home/Home';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+import Profile from './Components/Profile/Profile';
 import PrivateZone from './guards/PrivateZone';
-import NotFound from './components/NotFound/NotFound';
+import NotFound from './Components/NotFound/NotFound';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
