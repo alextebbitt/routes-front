d5e939ae461cc52af1391fcea10b5c077b243dbf
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-16 19:09:26 +0200

    basic header structure

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { notification, Avatar } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../features/auth/authSlice";
+import {LogoutOutlined, HomeOutlined,} from "@ant-design/icons";
+import { Link, useNavigate } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
 
-export default Header
+  const onLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    notification.success({ message: "logged out successfully" });
+
+    navigate("/login");
+  };
+
+  return (
+    <nav>
+      <div className="topnav">
+        <img src="" height="80px" alt="logo"></img>
+
+        <div className="headerstuff">
+          <span>
+            <Link to="/">
+              <HomeOutlined />
+            </Link>
+          </span>
+          {user ? (
+            <>
+              <span>
+                <Link to="/" onClick={onLogout}>
+                  {<LogoutOutlined />}
+                </Link>
+              </span>
+              <span>
+                <Link to="/profile">
+                  <Avatar size={"large"} style={{ backgroundColor: "#feb93f" }}>
+                    {user.user.name[0]}
+                  </Avatar>
+                </Link>
+              </span>
+              {user.user.role === "admin" ? (
+                <span>
+                  <Link to="/admin">Admin</Link>
+                </span>
+              ) : (
+                ""
+              )}
+            </>
+          ) : (
+            <>
+              <span>
+                <Link to="/login">Login</Link>
+              </span>
+              <span>
+                <Link to="/register">Register</Link>
+              </span>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
