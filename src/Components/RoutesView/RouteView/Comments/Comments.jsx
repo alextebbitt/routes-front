9c7954e93fc428672d0d5392127751c27605d465
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-20 12:21:00 +0200

    comment map

--- src/Components/RoutesView/RouteView/Comments/Comments.jsx
+++ src/Components/RoutesView/RouteView/Comments/Comments.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {
+  HeartOutlined,
+  HeartFilled,
+  DeleteOutlined,
+  EditOutlined,
+  MessageOutlined,
+} from "@ant-design/icons";
 
 const Comments = () => {
+
+const { comments } = useSelector(state => state.routes);
+
+const comment = comments.map((comment) => {
+return (
+    <div className='commentbody'>{comment}</div>
+)
+})
+
   return (
-    <div>Comments</div>
+    <div>{comments}</div>
   )
 }
 
