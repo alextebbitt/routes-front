9ef7ee2487308a2741a25dc7703d60256d65504e
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-20 12:01:51 +0200

    comments service done

--- src/features/comments/commentsService.js
+++ src/features/comments/commentsService.js
@@ -6,8 +6,46 @@ const getComments = async (page = 1) => {
     return res.data;
 };
 
+const createComment = async (commentData) => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    const res = await axios.post(API_URL + "/comments/create", commentData, {
+        headers: {
+            authorization: user?.token
+        }
+    });
+
+    return res.data
+};
+
+const deleteComment = async (id) => {
+
+    const user = JSON.parse(localStorage.getItem("user"));
+    const res = await axios.delete(API_URL + "/comments/" + id, {
+        headers: {
+            authorization: user?.token,
+        },
+    });
+    return res.data;
+};
+
+const updateComment = async (comment) => {
+
+    const user = JSON.parse(localStorage.getItem("user"));
+    const res = await axios.put(API_URL + "/comment/" + comment.id, comment, {
+        headers: {
+            authorization: user?.token
+        }
+    })
+
+
+    return res.data
+};
+
 const commentsService = {
-    getComments
+    getComments,
+    createComment,
+    deleteComment,
+    updateComment
 };
 
 export default commentsService; 
