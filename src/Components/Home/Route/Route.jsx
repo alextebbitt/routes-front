0100d8b0d37095ab9560683563a039e9c076a86c
Xavimat <xaviermatoses@gmail.com>
2022-07-24 19:56:05 +0200

    Solve conflicts

--- src/Components/Home/Route/Route.jsx
+++ src/Components/Home/Route/Route.jsx
@@ -1,32 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Route.scss";
-<<<<<<< HEAD
-import { StarOutlined, ClockCircleOutlined, HeartOutlined,HeartFilled } from "@ant-design/icons";
-import { addToWishlist, removeFromWishlist } from "../../../features/auth/authSlice";
-=======
-import { Skeleton } from "antd";
 import { StarOutlined, ClockCircleOutlined, HeartOutlined, HeartFilled } from "@ant-design/icons";
-
+import { addToWishlist, removeFromWishlist } from "../../../features/auth/authSlice";
+import { Skeleton } from "antd";
 const Route = ({ isLoadingRoutes }) => {
->>>>>>> develop
 
   const { routes } = useSelector((state) => state.routes);
-<<<<<<< HEAD
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-  const truncateAfterWord = (str, chars, placeholder = '...') =>  str.length < chars ? str : `${str.substr( 0, str.substr(0, chars - placeholder.length).lastIndexOf(" "))}${placeholder}`;
-  const route = routes.map((route) => {
-    console.log("routes", route._id)
-    console.log("user", user);
-    const isAlreadyInWishlist = user.user?.wishlist?.includes(route._id);
-=======
-
   const truncateAfterWord = (str, chars, placeholder = '...') => str.length < chars ? str : `${str.substr(0, str.substr(0, chars - placeholder.length).lastIndexOf(" "))}${placeholder}`;
 
   const route = routes.map((route) => {
-
->>>>>>> develop
+    // console.log("routes", route._id)
+    // console.log("user", user);
+    const isAlreadyInWishlist = user.user?.wishlist?.includes(route._id);
     const tag = route.tags?.map((tag, i) => (
       <>
         <Link key={tag + i + route._id} to={`/tag/${tag}`}>
@@ -44,18 +32,13 @@ const Route = ({ isLoadingRoutes }) => {
               src={route.image}
               alt={route.name} />
           </Link>
-
-<<<<<<< HEAD
           <div className="btn">
-            {isAlreadyInWishlist ? (
-              <HeartFilled className="icon" onClick={() => dispatch(removeFromWishlist(route._id))}/>
-              ) : (
-                <HeartOutlined className="icon" onClick={() => dispatch(addToWishlist(route._id))} />
-              )}
-             </div>
-=======
-          <div className="btn"> <HeartOutlined className="icon" /></div>
->>>>>>> develop
+            {isAlreadyInWishlist ?
+              <HeartFilled className="icon" onClick={() => dispatch(removeFromWishlist(route._id))} />
+              :
+              <HeartOutlined className="icon" onClick={() => dispatch(addToWishlist(route._id))} />
+            }
+          </div>
         </div>
         <div className="routeDescription">
           <div className="routeDetails">
