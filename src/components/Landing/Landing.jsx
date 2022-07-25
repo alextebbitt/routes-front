78f145149111a574f84ef5337ea9ddf4072f93e4
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-07-25 09:51:11 +0200

    styling landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {  useNavigate,Link } from "react-router-dom";
-
+import "./Landing.scss"
 
 const Landing = () => {
   return (
-    <div>Landing
-             <img src="" height="80px" alt="logo"></img>
+    <div className="landing">Landing
+    <div className="pictureWall">
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      <img src="https://i.imgur.com/TjLSq2l.jpg"  />
+      
+    </div>
+             <img src="https://i.imgur.com/TjLSq2l.jpg" height="80px" alt="logo"></img>
         <div className="gif">
             <img src="https://c.tenor.com/b5ye9Sj0hXQAAAAM/valencia-awkward.gif"/>
         </div>
