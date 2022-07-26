eb17001bd6740a4102ba6728a80ecb1ceb11e679
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-07-26 13:34:29 +0200

    editing login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,82 +1,87 @@
-import {React, useEffect} from 'react'
+import { React, useEffect } from "react";
 import { Button, Form, Input, notification } from "antd";
-import { useDispatch, useSelector } from 'react-redux';
-import {  useNavigate,Link } from "react-router-dom";
-import { login, reset} from '../../features/auth/authSlice';
-import "./Login.scss"
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import { login, reset } from "../../features/auth/authSlice";
+import "./Login.scss";
 const Login = () => {
-  const { isError, isSuccess, message} = useSelector((state) => state.auth)
+  const { isError, isSuccess, message } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-useEffect(() => {
-  if (isError) {
-    notification.error({message: "Error", description: message })
-  }
-  if (isSuccess) {
-    notification.success({ message: "Con éxito", description: message });
-  }
-  dispatch(reset());
-}, [isError, isSuccess, message])
-
+  useEffect(() => {
+    if (isError) {
+      notification.error({ message: "Error", description: message });
+    }
+    if (isSuccess) {
+      notification.success({ message: "Con éxito", description: message });
+    }
+    dispatch(reset());
+  }, [isError, isSuccess, message]);
 
   const onFinish = (values) => {
     dispatch(login(values));
     setTimeout(() => {
       navigate("/home");
     }, 1000);
-    
   };
 
   return (
-    < div className="login">
-      <div className="picture">
-     </div>
-      
-     <div className="loginForm"><Form
-        className="form"
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "¡Por favor introduce su correo electrónico!",
-            },
-          ]}
+    <div className="login">
+      <div className="container">
+        <div className="picture">
+        </div>
+        <div className="text">Hola caracola</div>
+      </div>
+
+      <div className="loginForm">
+        <Form
+          className="form"
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "¡Por favor introduce su correo electrónico!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          label="Contraseña"
-          name="password"
-          rules={[
-            { required: true, message: "¡Por favor introduce su contraseña!" },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label="Contraseña"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "¡Por favor introduce su contraseña!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item>
-          <Button className="submitbtn" htmlType="submit">
-            Enviar
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item>
+            <Button className="submitbtn" htmlType="submit">
+              Enviar
+            </Button>
+          </Form.Item>
+        </Form>
 
-      <span>
-        ¿Todavía no tienes cuenta? <Link to="/register">Regístrate</Link>
-      </span>
+        <span>
+          ¿Todavía no tienes cuenta? <Link to="/register">Regístrate</Link>
+        </span>
+      </div>
     </div>
-    </div> 
   );
-}
+};
 
-export default Login
+export default Login;
