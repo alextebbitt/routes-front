4976968c068b5a4556d63817c22a182a5ce0a107
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-07-26 10:59:26 +0200

    styling

--- src/components/RoutesNearBy/RoutesNearBy.jsx
+++ src/components/RoutesNearBy/RoutesNearBy.jsx
@@ -69,7 +69,6 @@ const RoutesNearBy = () => {
         zoom={17}
         scrollWheelZoom={false}>
         <TileLayer
-        className="copyright"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
