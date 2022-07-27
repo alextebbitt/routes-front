2f7f5bf5f06d47b38a29b3ea47ad3d19abbe63ad
Xavimat <xaviermatoses@gmail.com>
2022-07-27 18:12:59 +0200

    404 details

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button, Result } from "antd";
-import { HomeOutlined} from "@ant-design/icons";
-import { Link} from "react-router-dom";
-import "./NotFound.scss"; 
+import { HomeOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
 const NotFound = () => {
   return (
     <div>
-      <div className="container">
+      <div className="container404">
         <Result
           className="antd"
           status="404"
@@ -15,17 +15,14 @@ const NotFound = () => {
           extra={
             <Button className="button">
               <Link to="/home">
-                <div><HomeOutlined/></div>
+                <div><HomeOutlined /></div>
               </Link>
             </Button>
           }
         />
-
-        {/* <img className="gif"
-        src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif"
-        alt="description of gif" */}
-
-        {/* /> */}
+        <img className="gif"
+          src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif"
+          alt="description of gif" />
       </div>
     </div>
   );
