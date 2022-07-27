b04ce3622e65ee343c9f27f903f726b1af373d89
alextebbitt <alex.tebbitt87@gmail.com>
2022-07-27 17:06:56 +0200

    first idea

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { Button, Result } from "antd";
+import "./NotFound.scss"; 
 const NotFound = () => {
   return (
     <div>
+      
       <h1>404 page not found.</h1>
+      {/* <img className="gif"
+        src="https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif"
+        alt="description of gif" */}
+        
+      {/* /> */}
     </div>
   );
 };
